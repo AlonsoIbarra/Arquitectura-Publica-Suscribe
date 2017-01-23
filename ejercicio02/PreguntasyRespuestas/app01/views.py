59f90f84ce3f979f23c0de3d6c5dddb011c841ed
Angel G <agustavo79@gmail.com>
2017-01-23 17:17:29 -0600

    Se despliegan todas las preguntas de la base de datos

--- ejercicio02/PreguntasyRespuestas/app01/views.py
+++ ejercicio02/PreguntasyRespuestas/app01/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from app01.models import Pregunta, Respuesta
 
 # Create your views here.
+def index(request):
+    preguntas = Pregunta.objects.all()
+    respuesta_string = "Preguntas <br/>"
+    respuesta_string += '<br/>'.join(["id: %s, asunto: %s"%(p.id, p.asunto) for p in preguntas])
+    return HttpResponse(respuesta_string)
