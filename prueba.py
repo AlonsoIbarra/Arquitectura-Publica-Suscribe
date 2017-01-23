e985be9f5fd7ffd99ab6e3d8c697c280de6835e4
Angel G <agustavo79@gmail.com>
2017-01-23 16:15:19 -0600

    Commit inicial de los proyectos Django

---
+++ prueba.py
@@ -0,0 +1,27 @@
+import math
+
+print("Hola mundo")
+print("dsds")
+
+
+l = []
+l.append(1)
+print(l)
+
+class clase:
+    def __init__(self):
+        pass
+
+    def otro(self):
+        pass
+
+class dir():
+    pass
+
+print("hola")
+
+class algo():
+    def __init__():
+        pass
+
+    
