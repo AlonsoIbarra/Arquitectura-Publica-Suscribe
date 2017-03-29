58b045ba3c4030b4e9529db50aeb45a3eb95a09a
luisantonioig <c-remix@hotmail.es>
2017-03-28 22:23:06 -0600

    Estándar de código

--- ejemplo.py
+++ ejemplo.py
@@ -1,19 +1,19 @@
-#from contexto.Usuario import Usuario
-#from datos.ListaDeUsuarios import ListaDeUsuarios
+# from contexto.Usuario import Usuario
+# from datos.ListaDeUsuarios import ListaDeUsuarios
 
-#u = Usuario()
-#u.nombres="angel"
-#u.apellidos="lazaro"
-#u.edad=37
-#u.IDsTemperatura=1
-#u.IDsPresion=1
-#u.IDsAcelerometro=1
-#u.IDsRitmoCardiaco=1
+# u = Usuario()
+# u.nombres="angel"
+# u.apellidos="lazaro"
+# u.edad=37
+# u.IDsTemperatura=1
+# u.IDsPresion=1
+# u.IDsAcelerometro=1
+# u.IDsRitmoCardiaco=1
 
-#l = ListaDeUsuarios()
-#l.agregarUsuario(u)
+# l = ListaDeUsuarios()
+# l.agregarUsuario(u)
 
-#print(l.obtenerUsuarios())
+# print(l.obtenerUsuarios())
 
 from contexto.Medicamento import Medicamento
 from datos.ListaDeGrupos import ListaDeGrupos
@@ -21,8 +21,8 @@ from datos.ListaDeMedicamentos import ListaDeMedicamentos
 from contexto.Grupo import Grupo
 from datos.ListaDeUsuarios import ListaDeUsuarios
 
-#m = Medicamento()
-#m.descripcion = "Cloranfenamida"
+# m = Medicamento()
+# m.descripcion = "Cloranfenamida"
 
 l = ListaDeMedicamentos()
 m = l.obtenerMedicamentoPorId(1)
@@ -37,11 +37,11 @@ g.horaInicial = 12
 lg = ListaDeGrupos()
 lg.crearGrupo(g)
 
-#print(lg.obtenerGrupos())
+# print(lg.obtenerGrupos())
 
 l = ListaDeUsuarios()
 u = l.obtenerUsuarioPorId(1)
 print(u.nombres)
-#l.agregarMedicamento(m)
+# l.agregarMedicamento(m)
 
 #print(l.obtenerMedicamentos())
