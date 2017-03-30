916d126543c674a8f24e1f27a6ac2b3959f79192
Angel G <agustavo79@gmail.com>
2017-03-29 19:35:05 -0600

    Cambios de nombre de usuario a Miembro

---
+++ contexto/Miembro.py
@@ -0,0 +1,12 @@
+class Miembro():
+    idMiembro = 0
+    nombres = ""
+    apellidos = ""
+    edad = 0
+    IDsTemperatura = 0
+    IDsAcelerometro = 0
+    IDsRitmoCardiaco = 0
+    IDsPresion = 0
+
+    def __init__(self):
+        pass
