7ca70995355aaf16ea76b644146e7e4b71998856
AlonsoIbarra <saul.ibarra@cimat.mx>
2017-03-29 21:50:51 -0600

    Se agregaron funciones para eliminar y obtener grupos.

--- datos/ListaDeGrupos.py
+++ datos/ListaDeGrupos.py
@@ -18,9 +18,34 @@ class ListaDeGrupos():
                        str(grupo.medicamento.idMedicamento) + ")")
         con.cerrar()
 
+    def eliminarGrupo(self, grupo):
+        cn = Conexion()
+        cn.abrir()
+        cn.ejecutaSQL("delete from Grupos  where idGrupo =" + str(grupo.idGrupo) + "")
+        cn.cerrar()
+        return True
+
     def obtenerGrupos(self):
         con = Conexion()
         con.abrir()
         grupos = con.ejecutaSELECT("select * from Grupos")
         con.cerrar()
         return grupos
+
+    def obtenerGrupoPorId(self, idGrupo):
+        cn = Conexion()
+        cn.abrir()
+        resultado = cn.ejecutaBusqueda("select * from Grupos " +
+                                       "where idGrupo = " +
+                                       str(idGrupo))
+        grupo = self.__mapearGrupo(resultado)
+        cn.cerrar()
+        return grupo
+
+    def __mapearGrupo(self, presultado):
+        m = Grupo()
+        m.idGrupo = presultado[0]
+        m.periodo = presultado[1]
+        m.idMedicamento = presultado[2]
+        m.horaInicial = presultado[3]
+        return m
